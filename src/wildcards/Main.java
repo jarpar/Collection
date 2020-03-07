2b1e116add9133e3f2143d5d19d4db54b17e7cfc
jarpar <partyka.jaroslaw@gmail.com>
2020-03-07 19:18:56 +0100

    refactored

--- src/wildcards/Main.java
+++ src/wildcards/Main.java
@@ -1,7 +1,11 @@
 package wildcards;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<String> listOfStrings = new ArrayList<String>();
 
     }
 }
