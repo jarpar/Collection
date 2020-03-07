a8c5be40e649487c8a325f1a59410d7a56e82104
jarpar <partyka.jaroslaw@gmail.com>
2020-03-07 20:06:39 +0100

    refactored

--- src/genericMethods/Main.java
+++ src/genericMethods/Main.java
@@ -23,6 +23,24 @@ class Editor {
         }
     }
 
+    public static <T extends Figure> void print2(List<T> list) {
+        for (T f : list) {
+            System.out.println(f.getName());
+        }
+
+        list.add(null);
+
+        System.out.println("-------------------");
+
+        for (T f : list) {
+            if (f == null) {
+                System.out.println("null");
+                break;
+            }
+            System.out.println(f.getName());
+        }
+    }
+
     public static <Type> void MoveTableToCollection(Type[] tab, Collection<Type> c) {
         for (Type o : tab) {
             c.add(o);
@@ -58,11 +76,22 @@ public class Main {
     public static void main(String[] args) {
         Figure figure1 = new Circle();
         Figure figure2 = new Square();
+        Figure[] figureTab = new Figure[50];
 
         List<Figure> listOfFigures = new ArrayList<Figure>();
         listOfFigures.add(figure1);
         listOfFigures.add(figure2);
 
         Editor.print(listOfFigures);
+
+        Float[] fl = new Float[50];
+        Integer[] in = new Integer[50];
+        Number[] nu = new Number[50];
+        Collection<Number> cn = new ArrayList<Number>();
+        Editor.MoveTableToCollection(fl, cn);
+        Editor.MoveTableToCollection(in, cn);
+        Editor.MoveTableToCollection(nu, cn);
+        //Editor.MoveTableToCollection(figureTab, cn);
+
     }
 }
