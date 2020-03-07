8e85d30b708833609edbfe5f781c33b57b99b64b
jarpar <partyka.jaroslaw@gmail.com>
2020-03-07 19:37:14 +0100

    refactored

--- src/genericMethods/Main.java
+++ src/genericMethods/Main.java
@@ -1,5 +1,13 @@
 package genericMethods;
 
+import java.util.List;
+
+class editor {
+    public static void print(List<?> list) {
+
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
 
