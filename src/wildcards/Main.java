7662d7401dad02af99d0bcb5ca7004a1e271510b
jarpar <partyka.jaroslaw@gmail.com>
2020-03-07 19:21:53 +0100

    refactored

--- src/wildcards/Main.java
+++ src/wildcards/Main.java
@@ -2,10 +2,22 @@ package wildcards;
 
 import java.util.ArrayList;
 
+class SuperClass {
+
+}
+
+class SubClass extends SuperClass {
+
+}
+
 public class Main {
     public static void main(String[] args) {
 
         ArrayList<String> listOfStrings = new ArrayList<String>();
+
+        ArrayList<SuperClass> listOfClass = new ArrayList<SuperClass>();
+        listOfClass.add(new SuperClass());
+        listOfClass.add(new SubClass());
 
     }
 }
