6ccbc62eec600d4803a4dd0c4f3176fd2692ed46
jarpar <partyka.jaroslaw@gmail.com>
2020-03-07 18:18:22 +0100

    refactored

--- src/collection/Main.java
+++ src/collection/Main.java
@@ -1,10 +1,12 @@
 package collection;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class Main {
     public static void main(String[] args) {
-        Collection a;
+        Collection collection;
+        ArrayList arrayList;
 
     }
 }
