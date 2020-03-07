1dfcb1ba2fd8971541a204b6e740bbe8a3b3267d
jarpar <partyka.jaroslaw@gmail.com>
2020-03-07 18:34:49 +0100

    refactored

--- src/genericTypes/Main.java
+++ src/genericTypes/Main.java
@@ -1,7 +1,15 @@
 package genericTypes;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        String a;
+        ArrayList a = new ArrayList();
+        a.add(new StringBuffer("test"));
+        a.add(new Integer(7));
+
+        for (int i = 0; i < a.size(); i++) {
+            System.out.println(a.get(i));
+        }
     }
 }
