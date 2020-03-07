5ee0abcc6ad8551529145be489b0be5d38b32739
jarpar <partyka.jaroslaw@gmail.com>
2020-03-07 19:04:56 +0100

    refactored

--- src/genericTypes/Main.java
+++ src/genericTypes/Main.java
@@ -1,9 +1,9 @@
 package genericTypes;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
+
+        /*
         ArrayList a = new ArrayList();
         a.add(new String("test"));
         a.add(new Integer(7));
@@ -11,5 +11,23 @@ public class Main {
         for (int i = 0; i < a.size(); i++) {
             System.out.println(a.get(i));
         }
+        */
+
+        AnotherClass<String> somethingName;
+        somethingName.retVal("test2");
+
+        System.out.println(somethingName);
     }
+}
+
+class AnotherClass<TYPE> {
+    public void retVal(TYPE val) {
+        this.newVal = val;
+    }
+
+    public TYPE ret() {
+        return this.newVal;
+    }
+
+    TYPE newVal;
 }
